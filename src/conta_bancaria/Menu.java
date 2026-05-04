d664f3544bd6f56564ae81f1451b3a506aa08c83
yama-knz <igorkenzoyamamoto@gmail.com>
2026-05-04 13:09:10 -0300

    Conta corrente e poupança adicionada

--- src/conta_bancaria/Menu.java
+++ src/conta_bancaria/Menu.java
@@ -2,28 +2,31 @@ package conta_bancaria;
 
 import java.util.Scanner;
 
-import conta_bancaria.model.Conta;
+import conta_bancaria.model.ContaCorrente;
+import conta_bancaria.model.ContaPoupanca;
 import conta_bancaria.utils.Cores;
 
 public class Menu {
+	
+	private static final Scanner leia = new Scanner(System.in);
+	
 	public static void main(String[] args) {
-		
-		// Teste da Classe Conta
-        Conta c1 = new Conta(1, 123, 1, "Adriana Sanches", 10000.0f);
 
-        c1.visualizar();
+		// Teste da Classe Conta Corrente
+		ContaCorrente cc1 = new ContaCorrente(2, 123, 1, "José da Silva", 0.0f, 1000.0f);
+		cc1.visualizar();
+		cc1.sacar(12000.0f);
+		cc1.visualizar();
+		cc1.depositar(5000.0f);
+		cc1.visualizar();
 
-        c1.setSaldo(15000.0f);
-	    c1.setTitular("Maria Joaquina");
-		c1.visualizar();
-
-        c1.sacar(12000.0f);
-		c1.visualizar();
-
-        c1.depositar(5000.0f);
-		c1.visualizar();
-		
-		Scanner leia = new Scanner(System.in);
+		// Teste da Classe Conta Poupança
+		ContaPoupanca cp1 = new ContaPoupanca(3, 123, 2, "Maria dos Santos", 100000.0f, 15);
+		cp1.visualizar();
+		cp1.sacar(1000.0f);
+		cp1.visualizar();
+		cp1.depositar(5000.0f);
+		cp1.visualizar();
 
 		int opcao;
 
